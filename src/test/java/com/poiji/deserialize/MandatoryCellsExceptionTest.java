c7e8769bf081db3ba997cd96253ce7652612df77
Igor Volkov <virtual-machinist@users.noreply.github.com>
2024-03-05 15:54:37 +0200

    Test cases for no data in mandatory cell not causing PoijiMultiRowException

--- src/test/java/com/poiji/deserialize/MandatoryCellsExceptionTest.java
+++ src/test/java/com/poiji/deserialize/MandatoryCellsExceptionTest.java
@@ -5,14 +5,17 @@ import com.poiji.deserialize.model.byname.MandatoryMissingCells;
 import com.poiji.exception.PoijiMultiRowException;
 import com.poiji.exception.PoijiMultiRowException.PoijiRowSpecificException;
 import com.poiji.option.PoijiOptions;
-
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -27,6 +30,74 @@ public class MandatoryCellsExceptionTest {
             Poiji.fromExcel(createDummyExcel(), MandatoryMissingCells.class, PoijiOptions.PoijiOptionsBuilder
                     .settings()
                     .build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void testExcelMandatoryColumnFromXlsxFileNoCell() {
+        try {
+            Poiji.fromExcel(new File("src/test/resources/missing-cell-1.xlsx"), MandatoryMissingCells.class,
+                    PoijiOptions.PoijiOptionsBuilder.settings().build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void testExcelMandatoryColumnFromXlsxSheetNoCell() throws IOException, InvalidFormatException {
+        try (XSSFWorkbook workbook = new XSSFWorkbook(new File("src/test/resources/missing-cell-1.xlsx"))) {
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Poiji.fromExcel(sheet, MandatoryMissingCells.class, PoijiOptions.PoijiOptionsBuilder
+                    .settings()
+                    .build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void testExcelMandatoryColumnFromXlsxSheetBlankCell() throws IOException, InvalidFormatException {
+        try (XSSFWorkbook workbook = new XSSFWorkbook(new File("src/test/resources/missing-cell-2.xlsx"))) {
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Poiji.fromExcel(sheet, MandatoryMissingCells.class, PoijiOptions.PoijiOptionsBuilder
+                    .settings()
+                    .build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void testExcelMandatoryColumnFromXlsFileBlankCell() {
+        try {
+            Poiji.fromExcel(new File("src/test/resources/missing-cell-2.xls"), MandatoryMissingCells.class,
+                    PoijiOptions.PoijiOptionsBuilder.settings().build());
         } catch (PoijiMultiRowException e) {
             List<PoijiRowSpecificException> errors = e.getErrors();
             assertEquals(1, errors.size());
